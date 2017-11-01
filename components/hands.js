2691016827907052a1b043fa2a614f9c80b54b14
hessnd <n.hess@bigspaceship.com>
2017-11-01 19:36:57 -0400

    added all gifs and set default to black and white

--- components/hands.js
+++ components/hands.js
@@ -1,21 +1,28 @@
+import bangladeshBW from 'static/hands/bangladesh/BANGLADESH_BW.gif';
 import bangladesh from 'static/hands/bangladesh/BANGLADESH.gif';
-import centralEurope from 'static/hands/central-europe/C_Europe.gif';
+import centralEuropeBW from 'static/hands/central-europe/C_EUROPE_BW.gif';
+import centralEurope from 'static/hands/central-europe/C_EUROPE.gif';
+import chinaBW from 'static/hands/china/CHINA_BW.gif';
 import china from 'static/hands/china/CHINA.gif';
+import japanBW from 'static/hands/japan/JAPAN_BW.gif';
 import japan from 'static/hands/japan/JAPAN.gif';
+import northAmericaBW from 'static/hands/north-america/NA_BW.gif';
 import northAmerica from 'static/hands/north-america/NA.gif';
+import taiwanBW from 'static/hands/taiwan/TAIWAN_BW.gif';
 import taiwan from 'static/hands/taiwan/TAIWAN.gif';
+import otherBW from 'static/hands/other/Other_BW.gif'; 
 import other from 'static/hands/other/Other.gif'; 
 
 export default () => (
   <div>
     <section className="hands">
-      <img src={bangladesh} />
-      <img src={centralEurope} />
-      <img src={china} />
-      <img src={japan} />
-      <img src={northAmerica} />
-      <img src={taiwan} />
-      <img src={other} />
+      <img src={bangladeshBW} alt="bangladesh"/>
+      <img src={centralEuropeBW} alt="central europe"/>
+      <img src={chinaBW} alt="china"/>
+      <img src={japanBW} alt="japan"/>
+      <img src={northAmericaBW} alt="north america"/>
+      <img src={taiwanBW} alt="taiwan"/>
+      <img src={otherBW} alt="other"/>
     </section>
     <style jsx>{`
       .hands {
